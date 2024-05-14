485c6feef489a962971c5e9d0cbe64bfdf05f769
d0nc1h0t <udincev@yandex.ru>
2024-05-14 17:01:36 +0300

    Update freeimage source from svn to r1911 revision

--- src/Source/LibTIFF4/tif_win32.c
+++ src/Source/LibTIFF4/tif_win32.c
@@ -27,6 +27,10 @@
  * Scott Wagner (wagner@itek.com), Itek Graphix, Rochester, NY USA
  */
 
+#ifdef TIFF_DO_NOT_USE_NON_EXT_ALLOC_FUNCTIONS
+#undef TIFF_DO_NOT_USE_NON_EXT_ALLOC_FUNCTIONS
+#endif
+
 #include "tiffiop.h"
 #include <stdlib.h>
 
@@ -332,7 +336,7 @@ TIFF *TIFFOpenWExt(const wchar_t *name, const char *mode, TIFFOpenOptions *opts)
             dwMode = OPEN_EXISTING;
             break;
         case O_RDWR:
-            dwMode = OPEN_ALWAYS;
+            dwMode = OPEN_EXISTING;
             break;
         case O_RDWR | O_CREAT:
             dwMode = OPEN_ALWAYS;
